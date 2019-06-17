3a15ff74137816410d6e9f2cc725dcd156a8ca31
Mika312 <michaelpeltier31284@gmail.com>
2019-06-17 18:05:42 +0200

    amelioraion creation de message + debut du JQUERY pour recherche Message part username

--- script.js
+++ script.js
@@ -29,10 +29,20 @@ document.getElementById('btn-blue').addEventListener('click',function(){
     console.log("<== CLICK ADD ==>");
 
     // Recuperation de la valeur du champ de saisie.
-    var addMessage = document.getElementById('input').value;
+    var addMessage = document.getElementById('message').value;
+    var addfirstName = document.getElementById('firstName').value;
+    var addlastName = document.getElementById('lastName').value;
+    var addAvatar;
 
+    // Recuperation de l'avatar selectionné
+    for (var i = 0; i < document.getElementsByClassName('radioAvatar').length; i++) {
+        if ( document.getElementsByClassName('radioAvatar')[i].checked === true){
+            addAvatar = document.getElementsByClassName('radioAvatar')[i].value;
+        }
+    };
+
+    console.log("ADD AVATAR ==>", addAvatar)
     console.log("<== ADD MESSAGE ==>",addMessage);
-
     //Création div class='row' + Mise en place dans son element parent.
     var divRow = document.createElement("div");
     divRow.className = 'row';
@@ -41,27 +51,30 @@ document.getElementById('btn-blue').addEventListener('click',function(){
     //Création img class='avatar' + Mise en place dans son element parent.
     var avatar = document.createElement('img');
     avatar.className = 'avatar';
-    avatar.src = "avatar-1.jpg";
+    avatar.src = addAvatar;
     divRow.appendChild(avatar);
 
     //Création div + Mise en place dans son element parent.
     var divName = document.createElement("div");
+    divName.className = 'bodyMessage'
     divRow.appendChild(divName)
 
     //Création h6 firstName + Mise en place dans son element parent.
-    var firstName = document.createElement("h6")
-    firstName.textContent="Elise";
+    var firstName = document.createElement("h6");
+    firstName.className = 'firstName'
+    firstName.textContent= addfirstName.charAt(0).toUpperCase() + addfirstName.slice(1);
     divName.appendChild(firstName);
 
     //Création h6 lastName + Mise en place dans son element parent.
-    var lastName = document.createElement("h6")
-    lastName.textContent="Watson";
+    var lastName = document.createElement("h6");
+    lastName.className = 'lastName'
+    lastName.textContent= addlastName.toUpperCase();
     divName.appendChild(lastName);
 
     //Création paragraphe qui récupere le champ de saisie + Mise en place dans son element parent.
     var message = document.createElement('p');
     message.textContent = addMessage;
-    divRow.appendChild(message);
+    divName.appendChild(message);
 
     //Création img class='trash' + Mise en place dans son element parent.
     var trash = document.createElement('img');
@@ -70,7 +83,9 @@ document.getElementById('btn-blue').addEventListener('click',function(){
     divRow.appendChild(trash);
 
     //Remise à zéro du champ de saisie.
-    document.getElementById("input").value = "";
+    document.getElementById("message").value = "";
+    document.getElementById('firstName').value = "";
+    document.getElementById('lastName').value = "";
 
     //MàJ du compteur après création.
     document.getElementById('count').textContent=document.getElementsByClassName('row').length
@@ -85,3 +100,26 @@ document.getElementById('btn-blue').addEventListener('click',function(){
          });
 
 })
+
+// JQUERY
+
+$('#btn-search').click(function(){
+  console.log("SEARCH ===>",$('#searchBar').val());
+
+
+$(".bodyMessage").each(
+  function() {
+    console.log("FIRSTNAME",$(this).find('.firstName').text())
+    console.log("LASTNAME",$(this).find('.lastName').text())
+
+    if($(this).find('.firstName').text().toUpperCase() !==  $('#searchBar').val().toUpperCase() && $(this).find('.lastName').text().toUpperCase() !== $('#searchBar').val().toUpperCase()){
+        $(this).parent().fadeOut();
+    } else {
+        null
+    }
+  }
+);  
+
+$('#searchBar').val("") ;
+  
+});
